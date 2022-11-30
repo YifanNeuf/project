d88a320b9c376f8d7092ea9ba802a657fece9049
Yifan <qianmu0503@gmail.com>
2022-12-01 02:01:44 +0800

    發信：RUD完成

---
+++ src/list.js
@@ -0,0 +1,37 @@
+const charity = {
+    info: {
+        name: "",
+        mail: "",
+        status: "",
+        fundraisingNo: "",
+        tel: "",
+        registAddress: "",
+        contactAddress: "",
+        manager: {
+            name: "",
+            phone: ""
+        },
+        details: {
+            category: "",
+            concept: "",
+            intro: "",
+            authority: "",
+            demandPurpose: ""
+        }
+    },
+    file: {
+        time: "",
+        doc: {
+            application: "",
+            certificate: "",
+            permit: "",
+            affidavit: "",
+        },
+        img: {
+            logo: "",
+            photo: ""
+        }
+    },
+}
+
+console.log(charity);
